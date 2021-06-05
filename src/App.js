3b387c15f4fc2d22cab1b24ddc69820bac362113
Arif Hidayat <arief21.hidayat21@gmail.com>
2021-06-05 14:30:30 +0700

    #2.1 Spreading

--- src/App.js
+++ src/App.js
@@ -36,18 +36,10 @@ function App() {
       <header className="App-header">
         <h1> Company Directory </h1>
         {employeeInfo.map(employee => {
-          const {firstName, lastName, age} = employee;
           return (
-            <Employee firstName={firstName} lastName={lastName} age={age} />
+            <Employee {...employee} />
           );
-        })}
-        
-        
-        
-        {/* <Employee firstName="Jek" lastName="Smith" age="21" />
-        <Employee firstName="Arip" lastName="Gege" age="21" />
-        <Employee firstName="Dewa" lastName="Bumi" age="21" />
-        <Employee firstName="Sakti" lastName="Mandraguna" age="21" /> */}
+        })}        
       </header>
     </div>
   );
