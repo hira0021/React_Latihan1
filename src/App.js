08fbf33ee0c0d354365caff1e89c2e7ed182a880
Arif Hidayat <arief21.hidayat21@gmail.com>
2021-06-07 15:06:37 +0700

    #5 spread basic

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
 import React, { useState } from 'react';
 import './App.css';
-import EmployeeMap from './EmployeeMap';
-
-const employee = [
-  {
-    name: "Arip",
-    id: "1"
-  },
-  {
-    name: "Suhe",
-    id: "2"
-  },
-  {
-    name: "Jodi",
-    id: "3"
-  }
-];
 
 function App() {
+
+  const arr1 = [1, 2, 3];
+  const arr2 = [4, 5, 6];
+
+  const arr3 = [...arr1, ...arr2];
+
+  console.log(arr3);
 
   return (
     <div className="App">
       <header className="App-header">
-      
-      {employee.map(employee => 
-          <EmployeeMap key={employee.id} name={employee.name} id={employee.id} />
-      )}
-
-
 
       </header>
     </div>
