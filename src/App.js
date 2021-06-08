779c8da71559ab27b92d11a9aee4d19edcdee6bf
Arif Hidayat <arief21.hidayat21@gmail.com>
2021-06-08 20:06:53 +0700

    #6.2 Conditional Rendering (Ternary basic)

--- src/App.js
+++ src/App.js
@@ -3,27 +3,22 @@ import './App.css';
 
 function App() {
 
-
-  const employees = ["Hira", "Rean"];
-
-  // conditional rendering juga dapat digunakan dalam variable
-  // jadi dapat cek variabel dalam kasus employees undefined
-  const firstEmployees = employees && employees[0];
-
+  const isLogin = false;
+  
   return (
     <div className="App">
       <header className="App-header">
 
-      
-      {// penggunaan variabel (employees) didepan dalam kondisi akan mengembalikan true *dipakai untuk menghilangkan error meskipun variabel (employees) tidak ada isinya/undefined
-        // juga bisa menambahkan kondisi lain dengan menggunakan () contoh = employees && (employees.length > 0 || *kondisi lain*) && (jsx/hasil)
-        employees && employees.length > 0  && (
-          <div>
-          <h2>Employee List :</h2>
-          {employees.map(employees => (
-            <h4>{employees}</h4>
-          ))}
-          </div>
+      <h1>Welcome to the app!</h1>
+
+      {// Bentuknya = {Variabel ? (*apa yang dilakukan jika true*) : (apa yang dilakukan jika false)}
+        isLogin ? (
+        <h3>Hello Hira-san</h3>
+      ) : (
+        <div>
+          <h3>Please Login To Continue</h3>
+          <button>Login</button>
+        </div>
       )}
       
 
