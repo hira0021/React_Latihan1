69d7656b56c448bfab780a8b25ac544f96147451
Arif Hidayat <arief21.hidayat21@gmail.com>
2021-06-08 20:17:17 +0700

    #6.3 belajar menggabungkan state dan ternary

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import './App.css';
 
 function App() {
 
-  const isLogin = false;
+  // di coba menggunakan state 
+  const [isLogin, setIsLogin] = useState(true);
   
   return (
     <div className="App">
       <header className="App-header">
 
       <h1>Welcome to the app!</h1>
-
-      {// Bentuknya = {Variabel ? (*apa yang dilakukan jika true*) : (apa yang dilakukan jika false)}
+      <button onClick={() => setIsLogin(!isLogin)}>
+      {isLogin? (
+        "Logout"
+      ) : (
+        "Login"
+      )}
+    </button>
+      {
         isLogin ? (
         <h3>Hello Hira-san</h3>
       ) : (
-        <div>
-          <h3>Please Login To Continue</h3>
-          <button>Login</button>
-        </div>
+          <h3>Please Login</h3>
       )}
       
 
